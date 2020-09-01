feec488e4d6d074eef61d989aadd991a12bd145a
edgg72 <edgg72@hotmail.com>
2020-09-01 22:03:37 +0000

    adding 11

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,3 +1,3 @@
-#!/usr/bin/python3                                                                                                                                                                                                                      
-def add(a, b):
+#!/usr/bin/python3                                                                                                                                                                                                                     
+def pow(a, b):
     return(a ** b)
