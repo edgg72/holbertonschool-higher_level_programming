a2e876b9906b9539c1175bb05d75ee31566991b2
edgg72 <edgg72@hotmail.com>
2020-09-01 20:21:00 +0000

    removing one print

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 100):
     if(i != 99):
-        if i < 10:
-            print(0, sep="", end="")
-            print(i, " ",  sep=",", end="")
-        else:
-            print(i, " ",  sep=",", end="")
+        print("{:02d}".format(i), " ", sep=",", end="")
     else:
         print(i)
