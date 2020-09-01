dc939f54ac39db0512144b0194edb55a9eb09c13
edgg72 <edgg72@hotmail.com>
2020-09-01 20:10:11 +0000

    adding 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
-for i in range (0, 100):
-    if (i != 99):
-        print(i , " ",  sep=",", end="")
+for i in range(0, 100):
+    if(i != 99):
+        if i < 10:
+            print(0, sep="", end="")
+            print(i, " ",  sep=",", end="")
+        else:
+            print(i, " ",  sep=",", end="")
     else:
         print(i)
-
