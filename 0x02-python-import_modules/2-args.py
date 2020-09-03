8d7ccc0a525204e64ef761e54b3493adf7f2c8aa
edgg72 <edgg72@hotmail.com>
2020-09-03 17:36:39 +0000

    adding 1,2 and 3

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    av = len(argv) - 1
+    if av == 0:
+        print("0 arguments."))
+    else
+        print("{:d} {}".format(length, "arguments:"))
+        for i in range(1, av + 1):
+            print("{}: {}".format(i,sys.argv[i]))
