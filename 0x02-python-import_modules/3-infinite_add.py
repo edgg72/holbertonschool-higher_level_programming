ebdb9a370d35f90e1478f9709c53c0771120c3bc
edgg72 <edgg72@hotmail.com>
2020-09-03 18:06:59 +0000

    adding 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-import sys
+from sys import argv
 
 if __name__ == "__main__":
-    length = len(argv)
+    av = len(argv)
     sum = 0
-    for i in range(1, length):
+    for i in range(1, av):
         sum += int(argv[i])
     print("{:d}".format(sum))
+    
