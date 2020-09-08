38dc4683cd2a30c740235b4c11aa73c630815e29
edgg72 <edgg72@hotmail.com>
2020-09-08 18:47:35 +0000

    fix pep

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx < 0:
+    if idx >= 0 and idx <= len(my_list):
+        return my_list[idx]
+    else:
         return None
-    if idx > (len(my_list)):
-        return None
-
-    return my_list[idx]        
