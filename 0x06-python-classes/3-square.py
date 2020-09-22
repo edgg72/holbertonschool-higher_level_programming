3e35dfb3e2d91677fed9b463d171380ff9148764
edgg72 <edgg72@hotmail.com>
2020-09-22 22:00:24 +0000

    adding 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""definition of Square class"""
+
+
+class Square:
+    """Represents a square
+    Attributes:
+        __size: size of a square side
+    """
+    __size = None
+
+    def __init__(self, size=0):
+        """Initializes a square
+        Args:
+            size: size of a square side
+        Returns: None
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        else:
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
+
+    def area(self):
+        return (pow(self.__size, 2))
