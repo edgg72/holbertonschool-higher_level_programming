c1942330298283c0d0413e1cdea4cab107a55de7
edgg72 <edgg72@hotmail.com>
2020-09-22 23:13:32 +0000

    fixing setter

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -22,7 +22,7 @@ class Square:
     def size(self):
         return self.__size
 
-    @setter
+    @size.setter
     def size(self, value):
         if type(value) is not int:
             raise TypeError("size must be an integer")
