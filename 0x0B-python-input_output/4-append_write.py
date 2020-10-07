f6841ccc16b8677363ad7264742b42ef2856b7b6
edgg72 <edgg72@hotmail.com>
2020-10-07 22:06:20 +0000

    fixinf doc ex 4

--- 0x0B-python-input_output/4-append_write.py
+++ 0x0B-python-input_output/4-append_write.py
@@ -5,5 +5,6 @@ the append file function
 
 
 def append_write(filename="", text=""):
+    """returns the number of chars appended to a file"""
     with open(filename, 'a+', encoding='utf8') as f:
         return f.write(text)
