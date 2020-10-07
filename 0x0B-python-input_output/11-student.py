e702c6230448ec64caea7bfd00c8c9d1fc6752af
edgg72 <edgg72@hotmail.com>
2020-10-07 22:56:56 +0000

    adding 11

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
 """
+class Student
 """
 
 
 class Student:
-    """instantiation"""
+    """student"""
     def __init__(self, first_name, last_name, age):
+        """instantiation"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-    """to_json retrieves a dictionary representation of a Student instance"""
+
     def to_json(self):
+        """retrieves a dictionary representation of a Student instance"""
         return self.__dict__
