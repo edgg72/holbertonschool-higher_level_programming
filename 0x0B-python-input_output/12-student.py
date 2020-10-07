19eb68c4d815fdaa5d41f18f20cfc3c11d0ec25e
edgg72 <edgg72@hotmail.com>
2020-10-07 23:19:31 +0000

    adding 12

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+class Student
+"""
+
+
+class Student:
+    """student"""
+    def __init__(self, first_name, last_name, age):
+        """instantiation"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation of a Student instance"""
+        if type(attrs) is list:
+            new = {}
+            for i in range(len(attrs)):
+                if type(attrs[i]) is str:
+                    continue
+                else:
+                    break
+            for i in attrs:
+                try:
+                    new[i] = self.__dict__[i]
+                except:
+                    pass
+            return new
+        return self.__dict__
