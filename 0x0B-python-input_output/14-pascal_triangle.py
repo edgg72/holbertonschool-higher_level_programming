f16d3d5e9a412faacafbca67511077e9e982c331
edgg72 <edgg72@hotmail.com>
2020-10-08 02:54:47 +0000

    adding 14

---
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Pascal triangle
+"""
+
+
+def pascal_triangle(n):
+    """displays n lines of pascal triangle"""
+    triangle=[]
+    if n <= 0:
+        return triangle
+    for i in range(n):
+        triangle.append([])
+        triangle[i].append(1)
+        for j in range(1,i):
+            triangle[i].append(triangle[i-1][j-1]+triangle[i-1][j])
+        if(n!=0):
+            triangle[i].append(1)
+    return triangle
