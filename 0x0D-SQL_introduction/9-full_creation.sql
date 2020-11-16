588407dc7f016fdc1a69d667a7bf4a208ed7c9e3
edgg72 <edgg72@hotmail.com>
2020-11-16 18:16:59 -0500

    fixing 9

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -12,6 +12,4 @@
 --id = 3, name = “Bob”, score = 14
 --id = 4, name = “George”, score = 8
 CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
-UPDATE second_table
-SET 
 INSERT INTO second_table (id, name, score) VALUES (1, "John", 10), (2, "Alex", 3), (3, "Bob", 14), (4, "George", 8);
