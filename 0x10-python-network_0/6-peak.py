cba17eaafcad73e63df4254cba345f205d66fa10
edgg72 <edgg72@hotmail.com>
2020-12-18 14:36:56 -0500

    more efficiency for exercise 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,10 +4,14 @@
 
 def find_peak(list_of_integers):
     """function to find a peak"""
-    peak = 0
-    for i in list_of_integers:
-        if i > peak:
-            peak = i
-    if peak == 0:
-        return None
-    return peak
+    nums = list_of_integers
+    len_ = len(nums)
+    if len_ == 0:
+        return
+    m = len_ // 2
+    if (m == len_ - 1 or nums[m] >= nums[m + 1]) and\
+       (m == 0 or nums[m] >= nums[m - 1]):
+        return nums[m]
+    if m != len_ - 1 and nums[m + 1] > nums[m]:
+        return find_peak(nums[m + 1:])
+    return find_peak(nums[:m])
