74ad90bce10b28314e5f25a100f64112c68e3787
edgg72 <edgg72@hotmail.com>
2021-01-11 15:46:06 -0500

    importing differently

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,11 +2,11 @@
 """
 script that fetches https://intranet.hbtn.io/status
 """
-import urllib.request
+from urllib import request
 
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    with request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
 
     print("Body response:")
