f60979d8a791a83408360c402086f9fd16153d68
edgg72 <edgg72@hotmail.com>
2021-01-11 15:35:06 -0500

    adding if name == main

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -9,9 +9,10 @@ from urllib import request, parse
 from sys import argv
 
 
-email = argv[2]
-data_dict = {'email': email}
-data = parse.urlencode(data_dict).encode('utf-8')
+if __name__ == "__main__":
+    email = argv[2]
+    data_dict = {'email': email}
+    data = parse.urlencode(data_dict).encode('utf-8')
 
-with request.urlopen(argv[1], data) as response:
-    print(response.read().decode('utf8'))
+    with request.urlopen(argv[1], data) as response:
+        print(response.read().decode('utf8'))
