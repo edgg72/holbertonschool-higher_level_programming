845238e19c97454228b46ed238b7ff94492b4bd4
edgg72 <edgg72@hotmail.com>
2021-02-02 13:50:11 -0500

    working without length

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
 
-if (process.argv.length === 2) {
+if (process.argv[2] === undefined) {
   console.log('No argument');
-} else if (process.argv.length > 2) {
+} else {
   console.log(process.argv[2]);
 }
+
