101c8c9570b652eb81ea2008a2859884ec673b72
edgg72 <edgg72@hotmail.com>
2021-02-02 19:27:57 -0500

    fixing semistandard

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,10 +1,12 @@
 #!/usr/bin/node
 
 let secMax = 0;
-let numList = process.argv.slice(2);
+const numList = process.argv.slice(2);
 
 if (numList.length > 1) {
-  numList.sort(function(a, b){return a - b});
+  numList.sort(function (a, b) {
+    return a - b;
+  });
   secMax = numList[numList.length - 2];
 }
 
