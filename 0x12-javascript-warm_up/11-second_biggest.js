7edefa536e98173e544fb75656f390ce456e2de4
edgg72 <edgg72@hotmail.com>
2021-02-02 19:24:31 -0500

    fixing js sort problem

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,18 +1,11 @@
 #!/usr/bin/node
 
-if (isNaN(process.argv[2]) || process.argv.length < 4) {
-  console.log(0);
-} else {
-  const numList = [];
-  for (let i = 2; i < process.argv.length; i++) {
-    numList.push(parseInt(process.argv[i]));
-  }
-  const maxNum = Math.max(...numList);
-  let secMax = numList[0];
-  for (let i = 1; i < numList.length; i++) {
-    if (numList[i] > secMax && numList[i] < maxNum) {
-      secMax = numList[i];
-    }
-  }
-  console.log(secMax);
+let secMax = 0;
+let numList = process.argv.slice(2);
+
+if (numList.length > 1) {
+  numList.sort(function(a, b){return a - b});
+  secMax = numList[numList.length - 2];
 }
+
+console.log(secMax);
