0fc77fd0cf6b9ae5873430de1aa51070ec6a8b41
edgg72 <edgg72@hotmail.com>
2021-02-08 16:05:23 -0500

    adding 8 again without reverse

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,5 +1,9 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-  return list.reverse();
+  let newList = [];
+  for (let i = 0; i < list.length; i++) {
+    newList.unshift(list[i]);
+  }
+  return newList;
 };
