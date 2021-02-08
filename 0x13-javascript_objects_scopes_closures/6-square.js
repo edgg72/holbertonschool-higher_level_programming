95945d85d0dff3b99ec1e0d355153c21cc271e04
edgg72 <edgg72@hotmail.com>
2021-02-08 15:28:01 -0500

    adding 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const SquareFather = require('./5-square.js');
+
+class Square extends SquareFather {
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        console.log(c.repeat(this.width));
+      }
+    }
+  }
+}
+
+module.exports = Square;
