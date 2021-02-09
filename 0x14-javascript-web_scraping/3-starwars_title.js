fc8463d782237911b8d5e623c4a90f7b4c74ac22
edgg72 <edgg72@hotmail.com>
2021-02-09 14:18:45 -0500

    semistandard for 3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -5,6 +5,6 @@ const movie = process.argv[2];
 const req = 'https://swapi-api.hbtn.io/api/films/' + movie;
 
 request(req, function (error, response, body) {
-	if (error) return console.log(error);
-	console.log(JSON.parse(body).title);
+  if (error) return console.log(error);
+  console.log(JSON.parse(body).title);
 });
